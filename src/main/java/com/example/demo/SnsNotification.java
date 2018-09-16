b02b538f2c49a3adeed6e395ddd0a9c2e64feaa6
jfarr <jfarr1@godaddy.com>
2018-09-16 15:50:47 -0700

    cleanup

--- src/main/java/com/example/demo/SnsNotification.java
+++ src/main/java/com/example/demo/SnsNotification.java
@@ -6,14 +6,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SnsNotification {
 
-	@JsonProperty("Message")
-	private String message;
-	
-	public void setMessage(String message) {
-		this.message = message;
-	}
+  @JsonProperty("Message")
+  private String message;
 
-	public String getMessage() {
-		return message;
-	}
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
--- src/main/java/com/example/demo/SqsTestApplication.java
+++ src/main/java/com/example/demo/SqsTestApplication.java
@@ -19,65 +19,55 @@ import com.amazonaws.services.s3.event.S3EventNotification;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.example.demo.TestProtos.Message;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableSqs
 public class SqsTestApplication {
 
-	@Autowired
-	private AmazonS3 s3;
-	
-	ObjectMapper mapper = new ObjectMapper();
-	
-	@SqsListener("test-queue")
-	public void receive(String message) throws IOException, ClassNotFoundException {
-		List<S3EventNotificationRecord> records = readValue(
-				readValue(message, SnsNotification.class).getMessage(), 
-				S3EventNotification.class)
-			.getRecords();
-		for (S3EventNotificationRecord record : records) {
-			String key = record.getS3().getObject().getKey();
-			S3Object object = s3.getObject(new GetObjectRequest("f4908ccb-df04-42aa-b8a8-185daf9fdff1", key));
-			System.out.println(object);
-//			printProto(object.getObjectContent());
-			printString(object.getObjectContent());
-		}
-	}
-	
-	private void printString(InputStream objectContent) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(objectContent));
-		try {
-			String line = reader.readLine();
-			while (line != null) {
-				Object object = mapper.readValue(line, Map.class);
-				System.out.println(StringEscapeUtils.escapeJava(object.toString()));
-				line = reader.readLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  @Autowired
+  private AmazonS3 s3;
 
-	private void printProto(InputStream input) throws IOException {
-		Message message = TestProtos.Message.parseDelimitedFrom(input);
-		while (message != null) {
-			System.out.println(message.getText());
-			message = TestProtos.Message.parseDelimitedFrom(input);
-		}
+  ObjectMapper mapper = new ObjectMapper();
+
+  @SqsListener("test-queue")
+  public void receive(String message) throws IOException, ClassNotFoundException {
+    List<S3EventNotificationRecord> records = readValue(
+        readValue(message, SnsNotification.class).getMessage(), S3EventNotification.class)
+            .getRecords();
+    for (S3EventNotificationRecord record : records) {
+      String key = record.getS3().getObject().getKey();
+      S3Object object = s3
+          .getObject(new GetObjectRequest("f4908ccb-df04-42aa-b8a8-185daf9fdff1", key));
+      System.out.println(object);
+      printString(object.getObjectContent());
     }
-	
-	private <T> T readValue(String src, Class<T> valueType) {
-		try {
-			return mapper.readValue(src, valueType);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SqsTestApplication.class, args);
-	}
+  }
+
+  private void printString(InputStream objectContent) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(objectContent));
+    try {
+      String line = reader.readLine();
+      while (line != null) {
+        Object object = mapper.readValue(line, Map.class);
+        System.out.println(StringEscapeUtils.escapeJava(object.toString()));
+        line = reader.readLine();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private <T> T readValue(String src, Class<T> valueType) {
+    try {
+      return mapper.readValue(src, valueType);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SqsTestApplication.class, args);
+  }
 }
